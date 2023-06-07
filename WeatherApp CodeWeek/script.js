76dfc718619478a2444468c61a9bbadc8ec7fcea
Miles0908 <96233085+Miles0908@users.noreply.github.com>
2023-06-07 16:24:27 +0200

    7/06/2023

--- WeatherApp CodeWeek/script.js
+++ WeatherApp CodeWeek/script.js
@@ -4,11 +4,13 @@ const rootEl = document.querySelector("#root");
 
 const select = document.getElementById("selectProvince");
 
-let modal= document.getElementById("myModal");
+let modal = document.getElementById("myModal");
 
 let span = document.getElementsByClassName("close")[0];
 
 const searchWeatherEl = document.querySelector("inputText");
+
+let modalContent = document.querySelector(".modal-content");
 
 //CHIAMATE API
 
@@ -55,49 +57,58 @@ const cityGen = (weatherData) => {
 
   descriptionEl.textContent = weatherData.weather[0].description;
 
-  temperatureEl.textContent = `${(weatherData.main.temp - 273.15).toFixed(1)}°c`;
+  temperatureEl.textContent = `${(weatherData.main.temp - 273.15).toFixed(
+    1
+  )}°c`;
 
-  wrapperEl.addEventListener("click", ()=>{
-    openModal(weatherData)
-  })
-  
+  wrapperEl.addEventListener("click", () => {
+    openModal(weatherData);
+  });
+
+  wrapperEl.appendChild(iconEl);
   wrapperEl.appendChild(titleEl);
   wrapperEl.appendChild(temperatureEl);
   wrapperEl.appendChild(descriptionEl);
-  wrapperEl.appendChild(iconEl);
 
   //SE LA MODALE VIENE APERTA MOSTRA QUESTI ELEMENTI ADDIZIONALI
-  if (modal.style.display==="block"){
-    const minTempEl = document.createElement("h4"); 
-    const separatorEl= document.createElement("h4")
-    const separator2El= document.createElement("h4")
-    const separator3El= document.createElement("h4")
-    const separator4El= document.createElement("h4")
-    const maxTempEl = document.createElement("h4"); 
+  if (modal.style.display === "block") {
+    const minTempEl = document.createElement("h4");
+    const separatorEl = document.createElement("h4");
+    const separator2El = document.createElement("h4");
+    const separator3El = document.createElement("h4");
+    const separator4El = document.createElement("h4");
+    const maxTempEl = document.createElement("h4");
     const feelsLikeEl = document.createElement("h4");
-    const humidityEl = document.createElement("h4"); 
-    const windEl = document.createElement("h4"); 
-    
-    separatorEl.textContent=("//////////")
-    separator2El.textContent=("//////////")
-    separator3El.textContent=("//////////")
-    separator4El.textContent=("//////////")
-    minTempEl.textContent = `Temperatura minima: ${(weatherData.main.temp_min - 273.15).toFixed(1)}ºC`; 
-    maxTempEl.textContent = `Temperatura massima: ${(weatherData.main.temp_max - 273.15).toFixed(1)}ºC`; 
-    feelsLikeEl.textContent = `Temperatura percepita: ${(weatherData.main.feels_like - 273.15).toFixed(1)}ºC`; 
-    humidityEl.textContent = `Umidità: ${weatherData.main.humidity}%`; 
-    windEl.textContent = `Vento: ${weatherData.wind.speed} m/s`; 
+    const humidityEl = document.createElement("h4");
 
-    wrapperEl.classList.add("city-modal")
+    const windEl = document.createElement("h4");
 
-    wrapperEl.appendChild(minTempEl); 
-    wrapperEl.appendChild(separatorEl)
-    wrapperEl.appendChild(maxTempEl); 
-    wrapperEl.appendChild(separator2El)
-    wrapperEl.appendChild(feelsLikeEl); 
-    wrapperEl.appendChild(separator3El)
-    wrapperEl.appendChild(humidityEl); 
-    wrapperEl.appendChild(separator4El)
+    separatorEl.textContent = "//////////";
+    separator2El.textContent = "//////////";
+    separator3El.textContent = "//////////";
+    separator4El.textContent = "//////////";
+    minTempEl.textContent = `Temperatura minima: ${(
+      weatherData.main.temp_min - 273.15
+    ).toFixed(1)}ºC`;
+    maxTempEl.textContent = `Temperatura massima: ${(
+      weatherData.main.temp_max - 273.15
+    ).toFixed(1)}ºC`;
+    feelsLikeEl.textContent = `Temperatura percepita: ${(
+      weatherData.main.feels_like - 273.15
+    ).toFixed(1)}ºC`;
+    humidityEl.textContent = `Umidità: ${weatherData.main.humidity}%`;
+    windEl.textContent = `Vento: ${weatherData.wind.speed} m/s`;
+
+    wrapperEl.classList.add("city-modal");
+
+    wrapperEl.appendChild(minTempEl);
+    wrapperEl.appendChild(separatorEl);
+    wrapperEl.appendChild(maxTempEl);
+    wrapperEl.appendChild(separator2El);
+    wrapperEl.appendChild(feelsLikeEl);
+    wrapperEl.appendChild(separator3El);
+    wrapperEl.appendChild(humidityEl);
+    wrapperEl.appendChild(separator4El);
     wrapperEl.appendChild(windEl);
   }
   //END ELEMENTI ADDIZIONALI
@@ -119,6 +130,8 @@ const generateCards = async () => {
     2523650, //ragusa
     2523083, //siracusa
     2522876, //trapani
+    2517115, //Granada solo per fare vedere che funziona con la pioggia
+    4575352,//Columbia
   ];
 
   const weatherDataArray = await getWeatherData(idsArray);
@@ -140,30 +153,58 @@ select.addEventListener("change", (evt) => {
       rootEl.textContent = "";
       let card = cityGen(weatherDataArray.list[0]);
       card.classList.add("single-city");
-      
+
       rootEl.appendChild(card);
     });
   }
 });
 //END FILTRO
 
-
 //MODALE CHE SE CLICCATA UNA CARTA SI APRE
-let openModal=(weatherData)=>{
-  modal.style.display="block"
-  let modalContent =document.querySelector(".modal-content")
+let openModal = (weatherData) => {
+  modal.style.display = "block";
+  let modalContent = document.querySelector(".modal-content");
 
-  modalContent.appendChild(cityGen(weatherData))
+  modalContent.appendChild(cityGen(weatherData));
+  setBack(weatherData);
+};
+//
+//
+//CHIUDE E RESETTA IL CONTENUTO DELLA MODALE
+let closeModal = () => {
+  modal.style.display = "none";
+  let modalContent = document.querySelector(".modal-content");
+  let card = modalContent.querySelector(".city-modal");
+  modalContent.removeChild(card);
+  console.log("hi");
 };
 
-let closeModal=()=>{
-  modal.style.display="none"
-  let modalContent=document.querySelector(".modal-content")
-  modalContent.removeChild(card)
-};
-
-span.onclick= ()=>{
+span.onclick = () => {
   closeModal();
-}
-//END MODALE
+};
+//END CHIAMATE ALLA MODALE
 
+//INIZIO SETTING IMMAGINE BACKGROUND MODALE
+
+const setBack = (weatherData) => {
+  let weather = weatherData.weather[0].main.toLowerCase();
+
+  if (weather.includes("clear")) {
+    modalContent.style.background =
+      "url('./images/sunny-weather.jpg') no-repeat top / cover";
+    console.log("hi");
+  } else if (weather.includes("cloud")) {
+    modalContent.style.background =
+      "url('./images/cloudy-weather.jpg') no-repeat center center / cover";
+  } else if (weather.includes("rain")) {
+    modalContent.style.background =
+      "url('./images/rainy-weather.jpg') no-repeat center center / cover";
+  } else if (weather.includes("snow")) {
+    modalContent.style.background =
+      "url('./images/snowy-weather.jpg') no-repeat center center / cover";
+  } else {
+    modalContent.style.background =
+      "url('./images/default.jpg') no-repeat center center /cover";
+  }
+};
+//FINE BACKGROUND MODALE
